9a4ce31690e51edd64ce225d56e8df6691110c57
hemanthdugga <118503275+hemanthdugga@users.noreply.github.com>
2022-11-22 21:38:06 +0530

    Add files via upload

---
+++ A python program to print lower case alphabets using for loop.py
@@ -0,0 +1,469 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# A program to print letter 'a'
+a=1
+for a in range(6):
+    if a==1:
+        print("****** ")
+    elif a==2:
+        print("      *")
+    elif a==3:
+        print(" ******")
+    elif a==4:
+        print("*     *")
+    elif a==5:
+        print(" ***** *")
+
+
+# In[2]:
+
+
+# A program to print letter 'b'
+a=1
+for a in range(6):
+    if a==1:
+        print("*     ")
+    elif a==2:
+        print("*     ")
+    elif a==3:
+        print("***** ")
+    elif a==4:
+        print("*    *")
+    elif a==5:
+        print("*****")
+
+
+# In[3]:
+
+
+# A program to print letter 'c'
+a=1
+for a in range(6):
+    if a==1:
+        print("     ")
+    elif a==2:
+        print(" ****")
+    elif a==3:
+        print("*    ")
+    elif a==4:
+        print("*    ")
+    elif a==5:
+        print(" ****")
+
+
+# In[4]:
+
+
+# A program to print letter 'd'
+a=1
+for a in range(6):
+    if a==1:
+        print("    *")
+    elif a==2:
+        print("    *")
+    elif a==3:
+        print(" ****")
+    elif a==4:
+        print("*   *")
+    elif a==5:
+        print(" ****")
+
+
+# In[5]:
+
+
+# A program to print letter 'e'
+a=1
+for a in range(6):
+    if a==1:
+        print("     ")
+    elif a==2:
+        print(" **** ")
+    elif a==3:
+        print("******")
+    elif a==4:
+        print("*   ")
+    elif a==5:
+        print(" ****")
+
+
+# In[6]:
+
+
+# A program to print letter 'f'
+a=1
+for a in range(6):
+    if a==1:
+        print("   ***")
+    elif a==2:
+        print("  *  ")
+    elif a==3:
+        print(" **** ")
+    elif a==4:
+        print("  *  ")
+    elif a==5:
+        print("  *  ")
+
+
+# In[7]:
+
+
+# A program to print letter 'g'
+a=1
+for a in range(7):
+    if a==1:
+        print(" *** ")
+    elif a==2:
+        print("*   *")
+    elif a==3:
+        print(" ** *")
+    elif a==4:
+        print("    *")
+    elif a==5:
+        print("*   *")
+    elif a==6:
+        print(" *** ")
+
+
+# In[8]:
+
+
+# A program to print letter 'h'
+a=1
+for a in range(6):
+    if a==1:
+        print("*    ")
+    elif a==2:
+        print("*    ")
+    elif a==3:
+        print("**** ")
+    elif a==4:
+        print("*   *")
+    elif a==5:
+        print("*   *")
+
+
+# In[9]:
+
+
+# A program to print letter 'i'
+a=1
+for a in range(6):
+    if a==1:
+        print(" *")
+    elif a==2:
+        print("** ")
+    elif a==3:
+        print(" *")
+    elif a==4:
+        print(" *")
+    elif a==5:
+        print("  **")
+
+
+# In[10]:
+
+
+# A program to print letter 'j'
+a=1
+for a in range(6):
+    if a==1:
+        print("   * ")
+    elif a==2:
+        print("  **")
+    elif a==3:
+        print("   *")
+    elif a==4:
+        print("*  *")
+    elif a==5:
+        print(" ** ")
+
+
+# In[11]:
+
+
+# A program to print letter 'k'
+a=1
+for a in range(7):
+    if a==1:
+     print("*")
+    elif a==2:
+     print("*    *")
+    elif a==3:
+     print("*  * ")
+    elif a==4:
+     print("**")
+    elif a==5:
+     print("*  *")
+    elif a==6:
+     print("*    *")
+
+
+# In[14]:
+
+
+# A program to print letter 'l'
+a=1
+for a in range(6):
+    if a==1:
+        print("*")
+    elif a==2:
+        print("*")
+    elif a==3:
+        print("*")
+    elif a==4:
+        print("*")
+    elif a==5:
+        print("**")
+
+
+# In[15]:
+
+
+# A program to print letter 'm'
+a=1
+for a in range(6):
+    if a==1:
+        print("* *** ***")
+    elif a==2:
+        print(" *   *   *")
+    elif a==3:
+        print(" *   *   *")
+    elif a==4:
+        print(" *   *   *")
+
+
+# In[16]:
+
+
+# A program to print letter 'n'
+a=1
+for a in range(6):
+    if a==1:
+        print("")
+    elif a==2:
+        print("* ***")
+    elif a==3:
+        print(" *   *")
+    elif a==4:
+        print(" *   *")
+    elif a==5:
+        print(" *   *")
+
+
+# In[17]:
+
+
+# A program to print letter 'o'
+a=1
+for a in range(5):
+    if a==1:
+        print(" ***" )
+    elif a==2:
+        print("*   *")
+    elif a==3:
+        print("*   *")
+    elif a==4:
+        print(" ***")
+
+
+# In[18]:
+
+
+# A program to print letter 'p'
+a=1
+for a in range(7):
+    if a==1:
+        print("*")
+    elif a==2:
+        print(" ****")
+    elif a==3:
+        print(" *   *")
+    elif a==4:
+        print(" ****")
+    elif a==5:
+        print(" *")
+    elif a==6:
+        print(" *")
+
+
+# In[19]:
+
+
+# A program to print letter 'q'
+a=0
+for a in range(6):
+    if a==0:
+        print("     *")
+    elif a==1:
+        print(" ****")
+    elif a==2:
+        print("*   *")
+    elif a==3:
+        print(" ****")
+    elif a==4:
+        print("    * *")
+    elif a==5:
+        print("    *")
+
+
+# In[20]:
+
+
+# A program to print letter 'r'
+a=1
+for a in range(5):
+    if a==1:
+        print("* ***")
+    elif a==2:
+        print(" *  **")
+    elif a==3:
+        print(" *")
+    elif a==4:
+        print(" *")
+
+
+# In[21]:
+
+
+# A program to print letter 's'
+a=1
+for a in range(6):
+    if a==1:
+        print(" ****")
+    elif a==2:
+        print("*")
+    elif a==3:
+        print(" ****")
+    elif a==4:
+        print("     *")
+    elif a==5:
+        print(" ****")
+
+
+# In[22]:
+
+
+# A program to print letter 't'
+a=1
+for a in range(6):
+    if a==1:
+        print(" *")
+    elif a==2:
+        print("****")
+    elif a==3:
+        print(" *")
+    elif a==4:
+        print(" *")
+    elif a==5:
+        print("  **")
+
+
+# In[23]:
+
+
+# A program to print letter 'u'
+a=1
+for a in range(5):
+    if a==1:
+        print("*    *")
+    elif a==2:
+        print("*    *")
+    elif a==3:
+        print("*    *")
+    elif a==4:
+        print(" **** *")
+
+
+# In[24]:
+
+
+# A program to print letter 'v'
+a=1
+for a in range(5):
+    if a==1:
+        print("*     *")
+    elif a==2:
+        print(" *   *")
+    elif a==3:
+        print("  * *")
+    elif a==4:
+        print("   *")
+
+
+# In[25]:
+
+
+# A program to print letter 'w'
+a=1
+for a in range(6):
+    if a==1:
+        print("*   *   *")
+    elif a==2:
+        print("*   *   *")
+    elif a==3:
+        print("*   *   *")
+    elif a==4:
+        print(" **   **")
+
+
+# In[26]:
+
+
+# A program to print letter 'x'
+for a in range(6):
+    if a==1:
+        print("*     *")
+    elif a==2:
+        print("  * *")
+    elif a==3:
+        print("   *")
+    elif a==4:
+        print("  * *")
+    elif a==5:
+        print("*     *")
+
+
+# In[27]:
+
+
+# A program to print letter 'y'
+a=1
+for a in range(6):
+    if a==1:
+        print("*   *")
+    elif a==2:
+        print("*   *")
+    elif a==3:
+        print(" ****")
+    elif a==4:
+        print("    *")
+    elif a==5:
+        print("****")
+
+
+# In[28]:
+
+
+#A program to print 'z'
+a=1
+for a in range(6):
+    if a==1:
+        print("****")
+    elif a==2:
+        print("  *")
+    elif a==3:
+        print(" *")
+    elif a==4:
+        print("****")
+
+
+# In[ ]:
+
+
+
+
